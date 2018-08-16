5c30b8049bda7112e8d56d8e6020749fd5ca9b1d
MapleinCode <llkj13579@126.com>
2018-08-16 19:20:36 +0800

    feat: push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 自动实现鉴权透传
+
+## 默认参数
+
+1. data 传入的 data
+2. headers 
+3. body 
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const fs = require('fs');
+const cmdNames = fs.readdirSync('./lib/commands');
+
+
+
+// function dealCommands(commands) {
+
+// }
+
+function done(data = {}, commands, defined) {
+    const {
+        query,
+        body,
+        headers
+    };
+
+    
+};
+
+done({})
