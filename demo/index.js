b2d5bfa79f0a589d893425175ce551b8f62d78e5
MapleinCode <llkj13579@126.com>
2018-08-17 11:18:22 +0800

    feat: push

--- demo/index.js
+++ demo/index.js
@@ -8,6 +8,23 @@ const fs = require('fs');
 const path = require('path');
 
 const cmd = fs.readFileSync(path.resolve('./cmd'), { encoding: 'utf-8' });
+
 const define = require('./data.json');
-// console.log(cmd, define);
+
+const body = {
+    header: {
+        h: 'dd'
+    },
+    body: {
+        nihao: 'world'
+    },
+    query: {
+        caller: 'xiaofeidai'
+    }
+};
+
+const data = Object.assign(body, define);
+
+auto.done(data, cmd);
+
 
