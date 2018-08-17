a00cb9d81f18fd6d6026dc8145287a93aac288be
MapleinCode <llkj13579@126.com>
2018-08-17 11:18:30 +0800

    feat: push

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const path = require('path');
 const Variable = require('./lib/variable');
 const requireCommands = require('./lib/require_commands');
 
-function done(data = {}, commands, defined) {
-    // const {
-    //     query,
-    //     body,
-    //     headers
-    // } = data;
+function format(commands) {
+    const cmds = commands.split('\n').map(cmd => cmd.split(' '));
+    return cmds;
+}
 
+function done(data = {}, commands) {
     const variable = new Variable(data);
-
     
+    const cmds = format(commands);
+    console.log(cmds);
 };
 
 exports.done = done;
