aa75dbf7aa8dcb8d5f7f61278a67c7f68149377e
MapleinCode <llkj13579@126.com>
2018-08-17 11:33:39 +0800

    feat: push

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const Variable = require('./lib/variable');
 const requireCommands = require('./lib/require_commands');
 
 function format(commands) {
-    const cmds = commands.split('\n').map(cmd => cmd.split(' '));
+    const cmds = commands.split('\n');
+
+    const realCmds = [];
+
+    for(cmd of cmds) {
+        if(cmd[0] === '#') {
+            continue;
+        }
+
+        if(cmd.indexOf([]))
+    }
+
     return cmds;
 }
 
