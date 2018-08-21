29247131440ff3c2496ab286d1750363cc831e53
MapleinCode <llkj13579@126.com>
2018-08-21 16:28:24 +0800

    feat: 把 cmd 初始化和运算分离

--- demo/index.js
+++ demo/index.js
@@ -1,15 +1,11 @@
 'use strcit';
 
-const auto = require('../index');
-
-const done = auto.done;
-
+const AutoSign = require('../index');
 const fs = require('fs');
 const path = require('path');
-
 const cmd = fs.readFileSync(path.resolve('./cmd'), { encoding: 'utf-8' });
-
 const define = require('./data.json');
+const debug = require('debug')('demo-test');
 
 const body = {
     header: {
@@ -25,6 +21,23 @@ const body = {
 
 const data = Object.assign(body, define);
 
-auto.done(data, cmd);
+let length = 1000;
+
+debug('init object begin');
+const autoSign = new AutoSign(cmd);
+debug('init object end');
+
+debug('begin done 5000 times');
+while(length --) {
+    autoSign.done(data);
+}
+
+debug('end done 5000 times');
+
+
+
+
+
+
 
 
--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 'use strict';
 
-const Variable = require('./lib/variable');
+const Variable = require('./lib/variable'); // 变量集合
+const CMD = require('./lib/CMD'); // CMD 集合
 
-const CMD = require('./lib/CMD');
+class AutoSign {
+    constructor(cmds) {
+        this.cmds = this.init(cmds);
+    }
 
-function format(commands, variable) {
-    const cmds = commands.split('\n');
-    const realCmds = cmds.map(cmd => new CMD(cmd, variable));
-    return realCmds;
+    init(commands) {
+        const cmds = commands.split('\n').map(cmd => cmd.trim()).filter(cmd => !!cmd);
+        const realCmds = cmds.map(cmd => new CMD(cmd));
+        return realCmds;
+    }
+
+    done(data) {
+        const variable = new Variable(data);
+        for(const cmd of this.cmds) {
+            cmd.setVariable(variable);
+        }
+
+        let result;
+
+        for(const cmd of this.cmds) {
+            result = cmd.run();
+        }
+        return result;
+    }
 }
 
-function done(data = {}, commands) {
-    const variable = new Variable(data);
-    
-    const cmds = format(commands, variable);
-
-    for(const cmd of cmds) {
-        console.log(cmd.run());
-    }
-};
-
-exports.done = done;
+module.exports = AutoSign;
